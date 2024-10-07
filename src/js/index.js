56560cd44d6435799af49cb4ddec94d388e7cac5
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-10-07 20:37:11 +0500

    add slider with lib owl

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,21 @@
 'use strict'
 
+let owl
+
 $(document).ready(function(){
-    $('.carousel__inner').slick(
-        {
-            infinity: true,
-            speed: 1200,
-            adaptiveHeight: true,
-            prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/arrowleft.svg"></button>',
-            nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/arrowright.svg"></button>',
-            responsive: [
-                {
-                  breakpoint: 992,
-                  settings: {
-                    infinite: true,
-                    dots: false,
-                    arrows: false,
-                  }
-                },
-              ]
-        }
-    )
+  owl = $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    items: 1,
+    autoplay: true,
+  });
+});
+
+$('#next').click(function() {
+  owl.trigger('next.owl.carousel');
+})
+
+$('#prev').click(function() {
+  owl.trigger('prev.owl.carousel');
 })
