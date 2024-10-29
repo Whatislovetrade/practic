1d17605be283268223dd0871961ca8dc73a7ec2f
Vlad <vladmaslow1@gmail.com>
2024-10-29 21:16:27 +0500

    add mask and validate for form

--- src/js/index.js
+++ src/js/index.js
@@ -61,37 +61,38 @@ $(document).ready(function(){
     })
   })
 
-  //Validate
-  function validateForm(form) {
+  function validateForms(form){
     $(form).validate({
-      rules: {
-        name: {
-          required: true,
-          minlength: 2
-        },
-        phone: 'required',
-        email: {
-          required: true,
-          email: true
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
         },
         messages: {
-          name: {
-            required: "Пожалуйста введите свое имя",
-            minlength: jQuery.validator.format("Введите {0} символов!")
-          },
-          phone: 'Пожалуйста введите свой номер телефона',
-          email: {
-            required: "Пожалуйста введите свою почту",
-            email: "Неправильно введен адрес почты"
-          }
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
         }
-      }
-    })
-  }
+    });
+};
 
-  validateForm('#consultation form')
-  validateForm('#consultation-form')
-  validateForm('#order form')
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
 
