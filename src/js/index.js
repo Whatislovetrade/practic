a6d8725de543ee31c94f7a93354ad3ca7faf60c4
Vlad <vladmaslow1@gmail.com>
2024-10-30 23:03:20 +0500

    add scroll in header

--- src/js/index.js
+++ src/js/index.js
@@ -86,7 +86,7 @@ $(document).ready(function(){
             }
         }
     });
-};
+  };
 
 validateForms('#consultation-form');
 validateForms('#consultation form');
@@ -94,21 +94,53 @@ validateForms('#order form');
 
 
 
-$('form').submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-    }).done(function() {
-        $(this).find("input").val("");
-        $('#consultation, #order').fadeOut();
-        $('.overlay, #thanks').fadeIn('slow');
+  $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
 
-        $('form').trigger('reset');
-    });
-    return false;
-});
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+
+ // Scroll
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn()
+    } else {
+      $('.pageup').fadeOut()
+    }
+  })
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 300, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
 
 
